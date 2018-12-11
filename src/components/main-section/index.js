42f7cff55d3b5a4234605a3ccc28c5374b13dc83
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-12-11 15:47:46 +0100

    fixed 2 small css issues

--- src/components/main-section/index.js
+++ src/components/main-section/index.js
@@ -1,23 +1,12 @@
-import React from 'react'
+import React from 'react';
+import SearchFullScreen from '../search';
 
-import SearchFullScreen from '../search'
-
-const style = {
-	width: '100vw',
-	height: '100vh',
-	background: `url(https://source.unsplash.com/collection/135653)`,
-	backgroundSize: 'cover',
-	top: 0,
-	left: 0,
-	zIndex: -1,
-	opacity: 0.8,
-	overflow: 'hidden'
-}
+import './style.css';
 
 const MainSection = () => (
-	<div style={style}>
-		<SearchFullScreen />
-	</div>
-)
+  <div className="main-section">
+    <SearchFullScreen />
+  </div>
+);
 
-export default MainSection
+export default MainSection;
